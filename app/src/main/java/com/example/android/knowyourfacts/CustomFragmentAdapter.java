77b241bc77ae829792cc16cbc471181cacb63373
18045945 <aungmoethet2000@gmail.com>
2020-07-23 19:26:51 +0800

    Facts -> added

--- app/src/main/java/com/example/android/knowyourfacts/CustomFragmentAdapter.java
+++ app/src/main/java/com/example/android/knowyourfacts/CustomFragmentAdapter.java
@@ -1,4 +1,28 @@
 package com.example.android.knowyourfacts;
 
-public class CustomFragmentAdapter {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class CustomFragmentAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments;
+
+    public CustomFragmentAdapter(FragmentManager fm, ArrayList<Fragment> al) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragments = al;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
 }
--- app/src/main/java/com/example/android/knowyourfacts/MainActivity.java
+++ app/src/main/java/com/example/android/knowyourfacts/MainActivity.java
@@ -1,20 +1,62 @@
 package com.example.android.knowyourfacts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    CustomFragmentAdapter adapter;
+    ViewPager vPager;
+    Button btnRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRead = findViewById(R.id.btnReadLater);
+        vPager = findViewById(R.id.viewpager1);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new FragmentRandom());
+        al.add(new FragmentPrevious());
+        al.add(new FragmentNext());
+
+        adapter = new CustomFragmentAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.previous){
+
+        }
+        else if(item.getItemId() == R.id.random){
+
+        }
+        else {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
