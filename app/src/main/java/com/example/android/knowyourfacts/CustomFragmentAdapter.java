b692e254e1f8adc0e42ca7876e3b39a2d6486d5e
18045945 <aungmoethet2000@gmail.com>
2020-07-23 11:14:27 +0800

    Fragments -> created

---
+++ app/src/main/java/com/example/android/knowyourfacts/CustomFragmentAdapter.java
@@ -0,0 +1,4 @@
+package com.example.android.knowyourfacts;
+
+public class CustomFragmentAdapter {
+}
--- app/src/main/java/com/example/android/knowyourfacts/MainActivity.java
+++ app/src/main/java/com/example/android/knowyourfacts/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.knowyourfacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
